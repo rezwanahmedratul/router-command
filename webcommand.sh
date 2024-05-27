4ec46bac1b35a6bd37512e956e4f102f283e1262
rezwanahmedratul <rezwanahmedratul007@gmail.com>
2024-05-27 12:09:39 +0600

    update webcommand.sh

--- webcommand.sh
+++ webcommand.sh
@@ -5,9 +5,3 @@
 # reboot
 # service wpad restart
 # sh cpu_load_uptime.sh
-
-wget -P /tmp https://raw.githubusercontent.com/rezwanahmedratul/router-command/main/webconfig.sh
-
-mv /tmp/webconfig.sh /root/webconfig.sh
-chmod +x webconfig.sh
-reboot
