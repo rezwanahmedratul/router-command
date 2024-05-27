be54835c37cd87c99135931314b95688886a0b76
rezwanahmedratul <rezwanahmedratul007@gmail.com>
2024-05-28 00:01:12 +0600

    update webconfig.sh

--- webconfig.sh
+++ webconfig.sh
@@ -65,7 +65,7 @@ if [ $? -eq 0 ]; then
               mv /tmp/wireless /etc/config/wireless
               wifi reload radio0
               wifi reload radio1
-              service wpad restart
+            # service wpad restart
          fi
          
          
