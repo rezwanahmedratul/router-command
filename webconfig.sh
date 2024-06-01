4434d8ecd73701dfae5099e699eb5936d2fa63a6
rezwanahmedratul <rezwanahmedratul007@gmail.com>
2024-06-01 10:17:00 +0600

    update webconfig.sh

--- webconfig.sh
+++ webconfig.sh
@@ -63,8 +63,6 @@ if [ $? -eq 0 ]; then
               rm /tmp/wireless
          else
               mv /tmp/wireless /etc/config/wireless
-              /sbin/wifi reconf
-              /etc/init.d/wpad reload
               /etc/init.d/wpad restart
          fi
          
